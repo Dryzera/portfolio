5be2101e35bcacc967912455742c18238e258d8c
Dryzera <endryusdaniel6321@gmail.com>
2024-10-13 18:45:58 -0300

    general upgradins

--- static/js/screen_detector.js
+++ static/js/screen_detector.js
@@ -2,6 +2,5 @@ var width_device = screen.availWidth
 const element_display = document.getElementById("responsive")
 
 if (width_device < 1024) {
-    console.log('menor')
     element_display.style.display = "block"
 }
--- static/js/scroll-sense.js
+++ static/js/scroll-sense.js
@@ -1,14 +1,18 @@
-// const header = document.getElementById("top");
+const header = document.getElementById("top");
+const header_scroll = document.getElementById("header-scroll")
+const top_go = document.getElementById("back-to-top")
 
-// window.addEventListener('scroll', function() {
-//     // Verifica a posição do scroll
-//     if (window.scrollY > 0) {
-//         console.log("A página foi scrollada e não está mais no ponto 0.");
-//         header.style.position = "fixed";
-//         header.style.height = "1.8rem";
-//     } else {
-//         console.log("A página está no topo.");
-//         header.style.position = "relative";
-//         header.style.height = "10rem";
-//     }
-// });
+window.addEventListener('scroll', function() {
+    
+    if (window.scrollY >= 1) {
+        console.log("A página foi scrollada e não está mais no ponto 0.");
+        header.style.display = "none";
+        header_scroll.style.display = "block";
+        top_go.style.display = "block";
+    } else {
+        console.log("A página está no topo.");
+        header.style.display = "block";
+        header_scroll.style.display = "none";
+        top_go.style.display = "none";
+    }
+});
