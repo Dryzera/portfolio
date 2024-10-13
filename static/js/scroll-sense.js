b243df8eea9839764da407e77f6083070b67100f
Dryzera <endryusdaniel6321@gmail.com>
2024-10-12 23:08:52 -0300

    adjusted styles

---
+++ static/js/scroll-sense.js
@@ -0,0 +1,14 @@
+// const header = document.getElementById("top");
+
+// window.addEventListener('scroll', function() {
+//     // Verifica a posição do scroll
+//     if (window.scrollY > 0) {
+//         console.log("A página foi scrollada e não está mais no ponto 0.");
+//         header.style.position = "fixed";
+//         header.style.height = "1.8rem";
+//     } else {
+//         console.log("A página está no topo.");
+//         header.style.position = "relative";
+//         header.style.height = "5rem";
+//     }
+// });
