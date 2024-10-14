589cd7a3b343cc48cf7354c110ab9fd6885d56e8
Dryzera <endryusdaniel6321@gmail.com>
2024-10-13 22:55:01 -0300

    creating the contacts and projects page

--- static/js/scroll-sense.js
+++ static/js/scroll-sense.js
@@ -5,12 +5,12 @@ const top_go = document.getElementById("back-to-top")
 window.addEventListener('scroll', function() {
     
     if (window.scrollY >= 1) {
-        console.log("A página foi scrollada e não está mais no ponto 0.");
+        // console.log("A página foi scrollada e não está mais no ponto 0.");
         header.style.display = "none";
         header_scroll.style.display = "block";
         top_go.style.display = "block";
     } else {
-        console.log("A página está no topo.");
+        // console.log("A página está no topo.");
         header.style.display = "block";
         header_scroll.style.display = "none";
         top_go.style.display = "none";
