1bdcbc9141d1cf75d53cff5ea6e46428c015736b
Dryzera <endryusdaniel6321@gmail.com>
2025-01-20 18:38:13 -0300

    removed projects from html, inseted by js

--- static/js/auto_text.js
+++ static/js/auto_text.js
@@ -1,10 +1,8 @@
-function createProject(title, description, whats_used, date_creation, link) {
+function createProject(title, description, whats_used, date_creation, link, special) {
     const divProjects = document.querySelector('#grid-projects')
-
-    arrayLi = []
-    for(used of whats_used) {
-        template_li = `<li>${used}</li>`
-        arrayLi.push(template_li)
+    template_li = ''
+    for (used of whats_used) {
+        template_li += `<li>${used}</li>`
     };
 
     template = `
@@ -14,16 +12,79 @@ function createProject(title, description, whats_used, date_creation, link) {
     <p class="description"><b>Descrição:</b> ${description}</p>
     <ul>
         <h4>O que usei para desenvolver:</h4>
-
+        ${template_li}
     </ul>
     <p class="data-criacao">Desenvolvido em: ${date_creation}</p>
     <p class="link-project">Link para ver: <a href="${link}" target="_blank">clique aqui</a></p>
     `
     article = document.createElement('article')
     article.classList.add('project')
-    article.innerHTML = template
+
+    if (special) article.classList.add('project-star')
+
+    article.innerHTML += template
     divProjects.appendChild(article)
 };
 
-createProject('Teste', 'teste', ['js', 'py', 'dj'], '12/11', 'http://google.com')
+// tio elias's project
+createProject(
+    'Tio Elias Londrina', 
+    'foi criada uma simples landing page para divulgar um serviço, porém, sua maior funcionalidade é que ela é uma plataforma de gerenciamento de clientes, utilizada para: lançar e receber pagamentos, consultar análises, gerar relatórios e outros, são algumas das funcionalidades implementadas no site.', 
+    ['Django Framework', 'Python', 'HTML', 'CSS', 'Google Cloud Platform (GCP)', 'JavaScript', 'Nginx', 'Dominios e certificados SSL'], 
+    '09/2024', 
+    'https://www.tioeliaslondrina.com.br',
+    true
+)
 
+// ed planejamento project
+createProject(
+    'ED Planejamentos', 
+    'este é um site que permite que professores gerem planejamentos de aulas com o auxílio de uma IA, que é integrada ao gerar um planejamento.', 
+    ['Django Framework', 'Python', 'HTML', 'CSS', 'Google Cloud Platform (GCP)', 'JavaScript', 'Nginx', 'Dominios e certificados SSL'], 
+    '01/2025', 
+    'https://github.com/Dryzera/planejamentos-django-25',
+    true
+)
+
+// code defender project
+createProject(
+    'Code Defender', 
+    'um jogo simples, feito puramente em Python, criei para o teste de admissão na empresa Kodland.', 
+    ['Python'], 
+    '08/2024', 
+    'https://github.com/Dryzera/code-defender',
+    false
+)
+
+
+// meu portifolio project
+createProject(
+    'Meu Portfólio', 
+    'feito para uso pessoal e como forma de divulgar meu trabalho.', 
+    ['HTML', 'CSS', 'JavaScript'], 
+    '08/2024', 
+    'https://github.com/Dryzera/portfolio',
+    false
+)
+
+
+// e commerce project
+createProject(
+    'E-Commerce', 
+    'desenvolvido com django, este é um projeto de um simples e-commerce que explora diversas áreas do django, neste, em especial, sessions. Desenvolvido no curso de Python.', 
+    ['Python', 'Django Framework', 'SQL', 'Debug Toolbar', 'Bootstrap'], 
+    '11/2024', 
+    'https://github.com/Dryzera/e-commerce-django-2024',
+    false
+)
+
+
+// e commerce project
+createProject(
+    'Musicals Test', 
+    'desenvolvido TOTALMENTE por mim, tanto a parte de arquitetura, engenharia e programação do software em questão. O objetivo é treinar todos os conhecimentos obtidos durante o curso de python e aplicá-los neste projeto.', 
+    ['Python', 'Django Framework', 'SQL', 'Debug Toolbar', 'Bootstrap', 'JavaScript'], 
+    '11/24 - em dev', 
+    'https://github.com/Dryzera/musical-test-django-24',
+    false
+)
