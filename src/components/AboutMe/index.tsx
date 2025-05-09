589a20d6a08b7b26c4ed3dedb8aa63b16de6ddb0
Dryzera <endryusdaniel6321@gmail.com>
2025-05-09 20:03:53 -0300

    writing AboutMe component and commit favicons

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -4,18 +4,28 @@ import styles from './styles.module.css';
 export function AboutMe() {
   return (
     <Container animate={'fade-right'}>
-      <div className={styles.titleSect}>
-        <div className='linha'></div>
-        <h2>Sobre Mim</h2>
-        <div className='linha'></div>
-      </div>
-      <div className='content'>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor totam
-          animi facere, itaque voluptatibus dolorum esse natus tempore
-          asperiores, nemo ullam deleniti corporis. Hic, id esse adipisci rerum
-          repellendus facere.
-        </p>
+      <div className={styles.container}>
+        <div className={styles.titleSect}>
+          <div className='linha'></div>
+          <h2>Sobre Mim</h2>
+          <div className='linha'></div>
+        </div>
+        <div className={styles.content}>
+          <p>
+            Meu nome é Endryus, tenho 18 anos e atualmente estou no 3º ano do
+            ensino médio. Trabalho como tutor de programação, com foco em
+            Python, e dou aulas para iniciantes. Também estudo desenvolvimento
+            web, com conhecimentos em Django, HTML, CSS, JavaScript, TypeScript
+            e React. Tenho interesse em seguir carreira na área de tecnologia e
+            pretendo cursar Engenharia de Software. Além disso, sou dedicado aos
+            estudos, curioso por novas tecnologias e sempre em busca de aprender
+            mais para aprimorar meus projetos.
+          </p>
+          <p>
+            Também possuo algumas soft skills: trabalho em equipe, organização,
+            gestão de tempo, pensamento crítico, entre outras.
+          </p>
+        </div>
       </div>
     </Container>
   );
