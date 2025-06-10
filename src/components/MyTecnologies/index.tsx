fb822e3e92539e0ab81fbacfde7f7c9a9bc2111a
Dryzera <endryusdaniel6321@gmail.com>
2025-06-10 13:18:07 -0300

    added aria-label on imgs of MyTecnologies and added new technology (Next.js)

--- src/components/MyTecnologies/index.tsx
+++ src/components/MyTecnologies/index.tsx
@@ -19,27 +19,31 @@ export function MyTecnologies() {
                   src='/images/svgs/js.svg'
                   alt='Ícone do JavaScript'
                   title='JavaScript'
-                  aria-label=''
+                  aria-label='Ícone do JavaScript'
                 />
                 <img
                   src='/images/svgs/ts.svg'
                   alt='Ícone do TypeScript'
                   title='TypeScript'
+                  aria-label='Ícone do TypeScript'
                 />
                 <img
                   src='/images/svgs/react.svg'
                   alt='Ícone do React'
                   title='React'
+                  aria-label='Ícone do React'
                 />
                 <img
                   src='/images/svgs/html.svg'
                   alt='Ícone do HTML'
                   title='HTML'
+                  aria-label='Ícone do HTML'
                 />
                 <img
                   src='/images/svgs/css.svg'
                   alt='Ícone do CSS'
                   title='CSS'
+                  aria-label='Ícone do CSS'
                 />
               </div>
             </div>
@@ -50,21 +54,31 @@ export function MyTecnologies() {
                   src='/images/svgs/python.svg'
                   alt='Ícone do Python'
                   title='Python'
+                  aria-label='Ícone do Python'
                 />
                 <img
                   src='/images/svgs/django.svg'
                   alt='Ícone do Django'
                   title='Django'
+                  aria-label='Ícone do Django'
                 />
                 <img
                   src='/images/svgs/node-js.svg'
                   alt='Ícone do Node.js'
                   title='Node.js'
+                  aria-label='Ícone do Node.js'
                 />
                 <img
                   src='/images/svgs/express.svg'
                   alt='Ícone do Express.js'
                   title='Express.js'
+                  aria-label='Ícone do Express.js'
+                />
+                <img
+                  src='/images/svgs/next-js.svg'
+                  alt='Ícone do Next.js'
+                  title='Next.js'
+                  aria-label='Ícone do Next.js'
                 />
               </div>
             </div>
@@ -75,21 +89,25 @@ export function MyTecnologies() {
                   src='/images/svgs/postgresql.svg'
                   alt='Ícone do PostgreSQL'
                   title='PostgreSQL'
+                  aria-label='Ícone do PostgreSQL'
                 />
                 <img
                   src='/images/svgs/mysql.svg'
                   alt='Ícone do MySQL'
                   title='MySQL'
+                  aria-label='Ícone do MySQL'
                 />
                 <img
                   src='/images/svgs/mongodb.svg'
                   alt='Ícone do MongoDB'
                   title='MongoDB'
+                  aria-label='Ícone do MongoDB'
                 />
                 <img
                   src='/images/svgs/redis.svg'
                   alt='Ícone do Redis'
                   title='Redis'
+                  aria-label='Ícone do Redis'
                 />
               </div>
             </div>
@@ -100,31 +118,37 @@ export function MyTecnologies() {
                   src='/images/svgs/git.svg'
                   alt='Ícone do Git'
                   title='Git'
+                  aria-label='Ícone do Git'
                 />
                 <img
                   src='/images/svgs/github.svg'
                   alt='Ícone do Github'
                   title='Github'
+                  aria-label='Ícone do Github'
                 />
                 <img
                   src='/images/svgs/nginx.svg'
                   alt='Ícone do Nginx'
                   title='Nginx'
+                  aria-label='Ícone do Nginx'
                 />
                 <img
                   src='/images/svgs/docker.svg'
                   alt='Ícone do Docker'
                   title='Docker'
+                  aria-label='Ícone do Docker'
                 />
                 <img
                   src='/images/svgs/linux.svg'
                   alt='Ícone do Linux'
                   title='Linux'
+                  aria-label='Ícone do Linux'
                 />
                 <img
                   src='/images/svgs/gcp.svg'
                   alt='Ícone da Google Cloud Platform'
                   title='Google Cloud Platform'
+                  aria-label='Ícone do Google Cloud Platform'
                 />
               </div>
             </div>
