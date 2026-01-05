06f1bb9d036f2db15e2d4e1a6d37836903aabfc1
Dryzera <endryusdaniel6321@gmail.com>
2026-01-05 15:32:51 -0300

    Refactor code structure for improved readability and maintainability and upload a formal resume

--- src/components/Curriculum/index.tsx
+++ src/components/Curriculum/index.tsx
@@ -5,6 +5,7 @@ import { toast } from 'react-toastify';
 
 export function Curriculum() {
   const [hidden, setHidden] = useState(false);
+  const [showFormalResume, setShowFormalResume] = useState(true);
 
   function closeModal(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
     const { id } = e.target as HTMLElement;
@@ -27,31 +28,76 @@ export function Curriculum() {
             className={styles['close-button']}
             onClick={() => setHidden(false)}
           />
-
-          <img src='/documents/curriculo_endryus-daniel.png' />
-
-          <div className={styles['download-session']}>
-            <h3>Baixar</h3>
-
-            <a
-              href='/documents/curriculo_endryus-daniel.png'
-              download={true}
-              onClick={downloadMessage}
-              aria-label='Baixar curriculo em .png'
-              title='Baixar curriculo em .png'
+          <div className={styles['select-resume']}>
+            <span
+              onClick={() => setShowFormalResume(true)}
+              className={`${styles['select-option']} ${
+                showFormalResume && styles['selected-option']
+              }`}
             >
-              .png
-            </a>
-            <a
-              href='/documents/curriculo_endryus-daniel.pdf'
-              download={true}
-              onClick={downloadMessage}
-              aria-label='Baixar curriculo em .pdf'
-              title='Baixar curriculo em .pdf'
+              formal
+            </span>
+            <span
+              onClick={() => setShowFormalResume(false)}
+              className={`${styles['select-option']} ${
+                !showFormalResume && styles['selected-option']
+              }`}
             >
-              .pdf
-            </a>
+              social
+            </span>
           </div>
+
+          {!showFormalResume ? (
+            <div
+              className={`${styles['resume-flex']} ${styles['section-resume-display']} `}
+            >
+              <img src='/documents/curriculo_endryus-daniel.png' />
+
+              <div className={styles['download-session']}>
+                <h3>Baixar Currículo Social</h3>
+
+                <a
+                  href='/documents/curriculo_endryus-daniel.png'
+                  download={true}
+                  onClick={downloadMessage}
+                  aria-label='Baixar curriculo em .png'
+                  title='Baixar curriculo em .png'
+                >
+                  .png
+                </a>
+                <a
+                  href='/documents/curriculo_endryus-daniel.pdf'
+                  download={true}
+                  onClick={downloadMessage}
+                  aria-label='Baixar curriculo em .pdf'
+                  title='Baixar curriculo em .pdf'
+                >
+                  .pdf
+                </a>
+              </div>
+            </div>
+          ) : (
+            <div className={`${styles['section-resume-display']}`}>
+              <div className={styles['download-session']}>
+                <h3>Baixar Currículo Formal</h3>
+
+                <a
+                  href='/documents/endryus-formal-resume.pdf'
+                  download={true}
+                  onClick={downloadMessage}
+                  aria-label='Baixar curriculo formal em .pdf'
+                  title='Baixar curriculo formal em .pdf'
+                >
+                  .pdf
+                </a>
+              </div>
+
+              <div className={styles['formal-resume-images']}>
+                <img src='/documents/endryus-formal-resume-1.png' />
+                <img src='/documents/endryus-formal-resume-2.png' />
+              </div>
+            </div>
+          )}
         </div>
       </div>
 
